a4e539eccfbd20ebaa2d14eee43c75dc062341e8
MGX DEV <mgx.coding@gmail.com>
2022-12-05 10:44:40 +0100

    Added default routing, list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './common/error/error.component';
+import { ListComponent } from './recipes/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'error', component: ErrorComponent },
+  // Redirections always last
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: '**', redirectTo: 'error', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './recipes/list/list.component';
+import { ErrorComponent } from './common/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
