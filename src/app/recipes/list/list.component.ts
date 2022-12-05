f8cb62b23c8ce22d06fad343b3b458f51e2e5bbd
MGX DEV <mgx.coding@gmail.com>
2022-12-05 11:05:37 +0100

    Injected service into component

--- src/app/recipes/list/list.component.ts
+++ src/app/recipes/list/list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipesService } from '../../providers/http/recipes.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private service: RecipesService) {}
 
   ngOnInit(): void {
+    this.service.getRecipes().subscribe((recipes) => {
+      console.log(recipes);
+    });
   }
-
 }
